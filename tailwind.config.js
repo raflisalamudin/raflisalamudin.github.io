e966b42763fdc299865cd6e1219bf4ef468f1e1f
raflisalamudin <ahmadraflisalamudin1928@gmail.com>
2023-08-23 15:48:27 +0700

    menambahkan project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
   theme: {
     container: {
       center: true,
@@ -14,5 +14,5 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [require("flowbite/plugin")],
 };
